6bdc466faaca1ff7abec0148eb38cfd1821805b1
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-05-03 17:50:24 +0530

    app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import Editor from "./Editor";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Editor />
+    </div>
+  );
+};
+
+export default App;
