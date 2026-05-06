920e2dc39109729f32d094b02499598a66d280bf
ayushman sikder <ayushmansikder.ai@gmail.com>
2026-05-06 17:21:08 +0530

    wpm added

--- frontend/src/Editor.tsx
+++ frontend/src/Editor.tsx
@@ -25,6 +25,7 @@ const Editor: React.FC = () => {
     const totalTime = Math.floor((Date.now() - startTime) / 1000);  // total time in seconds
     const totalWords = currentBody.trim().split(" ").filter(Boolean).length;  // total words
     const totalCharacters = currentBody.length;  // total characters
+    const typingSpeed = Math.round((totalWords / totalTime) * 60); // typing speed in words per minute
 
     // Do nothing if both title and body are completely empty
     if (currentTitle === "Untitled" && currentBody.trim().length === 0) return;
@@ -41,6 +42,7 @@ const Editor: React.FC = () => {
             totalTime,
             totalWords,
             totalCharacters,
+            typingSpeed,
         },
       });
       
